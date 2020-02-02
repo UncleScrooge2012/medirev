184776269b585f3dc3a9de1a81ac9236d825036b
UncleScrooge2012 <naruk2024@gmail.com>
2020-02-03 00:13:34 +0300

    add section services and record and advantages and doctors

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,22 @@
 $(function(){
 
-  $('.slider__inner').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
-  });
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters    
+    loop: true,
 
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+
+  })
   
 });
